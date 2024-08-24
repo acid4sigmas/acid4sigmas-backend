a774b952dddeb855f6b5eb9abf46ace0a71f2bf0
klover <joelkerbusch@gmail.com>
2024-08-24 19:24:48 +0200

    added cache for api/me

--- src/api/me.rs
+++ src/api/me.rs
@@ -1,30 +1,40 @@
 use actix_web::{get, HttpMessage, HttpRequest, HttpResponse};
 use serde_json::json;
-use crate::{auth::utils::Claims, db::api::me::Database, error_response};
+use crate::{auth::utils::Claims, cache::init_caches::USER_ME_CACHE, db::api::me::Database, error_response};
 
 #[get("/me")]
 pub async fn me(req: HttpRequest) -> HttpResponse {
     let claims = req.extensions().get::<Claims>().cloned().unwrap();
 
-    let db = match Database::new().await {
-        Ok(db) => db,
-        Err(e) => return error_response!(500, e.to_string())
+    let user_id = match claims.user_id.parse::<i64>() {
+        Ok(uid) => uid,
+        Err(e) => error_response!(500, e.to_string())
     };
 
-    match db.create_table().await {
-        Ok(()) => (),
-        Err(e) => return error_response!(500, e.to_string())
-    }
-
-    let user_details = match db.read_by_uid(claims.user_id.parse().unwrap()).await {
-        Ok(user) => user,
-        Err(e) => return error_response!(500, e.to_string())
-    };
-
-    if let Some(usr_details) = user_details {
-        return HttpResponse::Ok().json(usr_details);
+    let cache = &*USER_ME_CACHE;
+    if let Some(user) = cache.get(&user_id) {
+        return HttpResponse::Ok().json(user);
     } else {
-        return error_response!(404, "couldnt find a user with this uid");
+        let db = match Database::new().await {
+            Ok(db) => db,
+            Err(e) => return error_response!(500, e.to_string())
+        };
+    
+        match db.create_table().await {
+            Ok(()) => (),
+            Err(e) => return error_response!(500, e.to_string())
+        }
+    
+        let user_details = match db.read_by_uid(claims.user_id.parse().unwrap()).await {
+            Ok(user) => user,
+            Err(e) => return error_response!(500, e.to_string())
+        };
+    
+        if let Some(usr_details) = user_details {
+            return HttpResponse::Ok().json(usr_details);
+        } else {
+            return error_response!(404, "couldnt find a user with this uid");
+        }
     }
     
 }
--- src/cache/init_caches.rs
+++ src/cache/init_caches.rs
@@ -1,7 +1,11 @@
-use crate::db::auth::auth::AuthUser;
+use crate::db::{api::me::User, auth::auth::AuthUser};
 
 use super::cache_manager::CacheManager;
 
 lazy_static::lazy_static! {
     pub static ref USER_CACHE: CacheManager<i64, AuthUser> = CacheManager::new(500);
+}
+
+lazy_static::lazy_static! {
+    pub static ref USER_ME_CACHE: CacheManager<i64, User> = CacheManager::new(500);
 }
